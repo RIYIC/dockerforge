2aace0ac5db7150bd0b02897815b6db1c68f0dba
J. Gomez <alambike@gmail.com>
2014-08-15 06:26:33 +0200

    Agregadas rutas que faltaban, Peticioins get xa devolve correctamente a representacion en json do obxeto (gracias ao metodo to_hash definido en  DFBase)

--- lib/dockerforge.pm
+++ lib/dockerforge.pm
@@ -1,5 +1,8 @@
 package dockerforge;
+use strict;
+
 use Dancer ':syntax';
+use DFUser;
 
 our $VERSION = '0.1';
 
@@ -14,13 +17,14 @@ hook before => sub {
     # todos menos a de creacion de usuario post#user
     unless($route->pattern =~ /\/user/ and $route->method eq 'post'){
 
-        unless(vars->{user} = DFUser->load(_id => params->{user})){
-
+        unless(vars->{user} = DFUser->load(UserId => params->{user})){
+            error "User ".params->{user}." not found!";
             send_error("Not allowed", 403);
 
         }
     }
-}
+};
+
 
 get '/' => sub {
     template 'index';
@@ -29,30 +33,30 @@ get '/' => sub {
 
 get 'queue/:job_id' => sub {
 
-    vars->{user}->jobs(_id => params->{job_id})
-}
+    [vars->{user}->jobs(_id => params->{job_id})]->[0]->to_hash;
+};
 
 
-# GET /dockers -> listar dockers
-get '/dockers' => sub {
+# GET /containers -> listar dockers
+get '/containers' => sub {
 
-    vars->{user}->containers;
+    [map {$_->to_hash} vars->{user}->containers];
 
-}
+};
 
-# GET /docker/:id -> obtener docker con id <id>
-get '/docker/:id' => sub {
+# GET /container/:id -> obtener docker con id <id>
+get '/container/:id' => sub {
 
-    vars->{user}->containers(Id => params->{id});
+    [vars->{user}->containers(Id => params->{id})]->[0]->to_hash;
     
-}
+};
 
 
-# POST /docker -> crear docker (ASINCRONA)
+# POST /container -> crear docker (ASINCRONA)
 #  - returns status: 202 Accepted, Location: /queue/<job_id> (http://restcookbook.com/Resources/asynchroneous-operations/)
 #  - GET /queue/:job_id : devolve o estado do traballo asincrono e os resultados que produxo se os hai
 #  Se aconsella que se o traballo e de creacion devolva un status 303 (see also) e un Location: <url ao novo recurso creado>
-post '/docker' => sub {
+post '/container' => sub {
     my $job_id = vars->{user}->createJob(
         "createContainer", 
         params
@@ -61,10 +65,10 @@ post '/docker' => sub {
     status 202;
 
     header('Location', "/queue/$job_id")
-}
+};
 
 # DELETE /docker/:id -> borra un container identificado polo id <id>
-del '/docker/:id' => sub {
+del '/container/:id' => sub {
 
     my $job_id = vars->{user}->createJob(
         "deleteContainer",
@@ -73,7 +77,7 @@ del '/docker/:id' => sub {
 
     status 202;
     header('Location', "/queue/$job_id");
-}
+};
 
 
 # GET /images -> lista as imaxenes dispoñibles que ten UN DETERMINADO CLIENTE
@@ -83,13 +87,13 @@ get '/images' => sub {
     my $system_user = DFUser->load(UserId => '0');
 
     ($system_user->images(public => 1), vars->{user}->images);
-}
+};
 
 # GET /image/:id -> obten os detalles da imaxen co id :id
 get '/image/:id' => sub {
 
-    vars->{user}->images(Name => params->{id})
-}
+    [vars->{user}->images(Name => params->{id})]->[0]->to_hash
+};
 
 # POST /image -> Crea a imaxen especificada e subea a todos os anfis (ou so aos do cliente) (ASINCRONA)
 post '/image' => sub {
@@ -102,22 +106,36 @@ post '/image' => sub {
     status 202;
     header('Location', "/queue/$job_id");
 
-}
+};
 
 # DELETE /image/:id -> Borrar unha imaxen especificada
-del '/image/:id' => sub {}
+del '/image/:id' => sub {
+    my $job_id = vars->{user}->createJob(
+        "deleteImage",
+        params
+    );
+
+    status 202;
+    header('Location', "/queue/$job_id");
+
+};
 
 
 # GET /hosts -> lista os hosts dispoñibles
-get '/hosts' => sub {}
+get '/hosts' => sub {         
+
+    [map {$_->to_hash} (vars->{user}->hosts)]
+};
 
 # GET /host/:id -> obten os detalles dun determinado host
-get '/host/:id' => sub {}
+get '/host/:id' => sub {
+    [vars->{user}->hosts(Hostname => params->{id})]->[0]->to_hash
+};
 
 # POST /host -> Da de alta na bbdd un novo host (para un determinado cliente)
-post '/host' => sub {}
+post '/host' => sub {};
 
 # DELETE /host/:id -> Elimina da bbdd un host
-del '/host/:id' => sub {}
+del '/host/:id' => sub {};
 
 true;
