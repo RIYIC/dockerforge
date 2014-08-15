ba5385c31305a5fb1c6cdc21fd943c295c981600
J. Gomez <alambike@gmail.com>
2014-08-15 06:27:25 +0200

    correxido o metodo getPendingJob Agregados novos atributos e relacions

--- lib/DFJob.pm
+++ lib/DFJob.pm
@@ -1,28 +1,39 @@
 package DFJob;
 
 use strict;
+use Eixo::Base::Clase;
 use parent qw(DFBase);
 
 
 has(
+    task => '',
     params => {},
     results => {},
-    # creation_timestamp => undef,
+    creation_timestamp => undef,
     start_timestamp => undef,
-    termination_timestamp => undef
-    running => undef,
-    done => undef,
-)
+    termination_timestamp => undef,
+    running => 0,
+    done => 0,
+
+    _id_DFUser => undef,
+);
+
+sub HAS_ONE {
+    user => 'DFUser'
+}
 
 sub getPendingJob{
     my ($class,%args) = @_;
 
-    &__collection($class)->find_and_modify(
-        query => {running => 0, done => 0},
+    my $doc = &DFBase::__collection($class)->find_and_modify({
+        query => {running => 0, done => 0, %args},
         sort => {_id => 1},
-        update => {running => 1, start_timestamp => time() },
-    )
+        update => {'$set' => {running => 1, start_timestamp => time()}},
+        new => 1,
+    });
+
+    ($doc)? $class->new(%$doc) : undef;
 
 }
 
-#Runner->(DFJob->getPendingJob)->run();
+1;
