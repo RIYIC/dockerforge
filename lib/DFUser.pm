7c3915353763bbfc34202199e0e5812f12ed6e4c
J. Gomez <alambike@gmail.com>
2014-08-15 06:33:08 +0200

    Creados metodos 'createJob' Esquematizado 'createHost' e 'remove' redefinido Definidas relacions

--- lib/DFUser.pm
+++ lib/DFUser.pm
@@ -3,6 +3,9 @@ package DFUser;
 use strict;
 use Eixo::Base::Clase;
 use parent qw(DFBase);
+use DFHost;
+use DFImage;
+use DFJob;
 
 has(
     UserId => undef,    
@@ -13,6 +16,49 @@ sub HAS_MANY {
 
     hosts => 'DFHost',
     images => 'DFImage',
-    jobs => 'DFJobs',
-    containers => 'DFContainers',
+    jobs => 'DFJob',
+    containers => 'DFContainer',
 }
+
+# Obligar a que todos os comandos que chegan pola api pasen polo user permite:
+# - que mediante a api so se acceda a aquelas entidades que ten accesibles (relacion) o usuario
+# - e moi facil introducir un sistema de permisos para controlar os comandos que pode lanzar un usuario, 
+
+
+
+sub remove {
+    my $self = $_[0];
+
+    # borrar hosts
+
+    # borrar containers
+
+    # borrar images
+
+    $self->SUPER::remove();
+}
+
+
+
+sub createHost{
+
+    my ($self,%args) = @_;
+
+    # testear conexion ca API de docker
+
+    DFHost->new(%args,_id_DFUser => $self->_id)->save;
+}
+
+
+sub createJob {
+    my ($self, $task, $params)  = @_;
+
+    DFJob->new(
+        task => $task, 
+        params => $params,
+        creation_timestamp => time(),
+        _id_DFUser => $self->_id
+    )->save;
+}
+
+sub testJob {sleep(int(rand(10)))}
