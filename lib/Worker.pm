652e46cba9bc363e1bd0507a7abd272313d626a3
J. Gomez <alambike@gmail.com>
2014-08-15 06:20:57 +0200

    Worker para correr jobs

---
+++ lib/Worker.pm
@@ -0,0 +1,57 @@
+package Worker;
+
+use strict;
+
+my $can_use_threads = eval 'use threads; 1';
+
+use Eixo::Base::Clase;
+use parent qw(Eixo::Base::Clase);
+
+my $MAX_PARALLEL_JOBS = 4;
+
+has(
+    jobClass => 'undef',
+    lap => 0
+);
+
+sub run {
+    my ($self, %args) = @_;
+
+    my %filters = ($self->jobClass)? (task => $self->jobClass) : ();
+
+    my $forever = !$args{shots};
+
+    # si damos mandado esto nun thread, poderiamos devolver o control do worker, para poder 
+    # matalo, ou facer outras tarefas
+    while($forever or $self->lap < $args{shots}){
+
+        while(my $job = DFJob->getPendingJob(%filters)){
+
+            $self->execJob($job);
+        }
+
+        $self->lap($self->lap + 1);
+
+        sleep(1);
+    }
+}
+
+sub execJob {
+    my ($self, $job) = @_;
+    my $user = DFUser->load(_id => $job->_id_DFUser);
+    my $method = $job->task;
+    
+    my $results = $user->$method(%{$job->params});
+
+    $job->results($results);
+    $job->termination_timestamp(time());
+    $job->done(1);
+    $job->save;
+}
+
+sub stop {
+    
+}
+
+
+
